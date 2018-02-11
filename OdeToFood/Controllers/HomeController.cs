9b527422fd1a2638f7c9b796e7f0924c3e7b0097
Sumit Prakash Mudkondwar <sumitmudkondwar@hotmail.com>
2018-02-11 08:17:06 +0530

    Add project files.

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var id = RouteData.Values["id"];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            var model = new AboutModel()
+            {
+                Name = "Sumit M",
+                Location = "Nagpur"
+            };
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
