cf1740a80dcb1ad807a78f807ace4f802dcb9765
Sumit Prakash Mudkondwar <sumitmudkondwar@hotmail.com>
2018-02-17 00:43:21 +0530

    first update

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OdeToFood.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,11 @@ namespace OdeToFood.Controllers
     public class CuisineController : Controller
     {
         // GET: Cuisine
+        //[Authorize]
+        [Log]
         public ActionResult Search(string name = "french")
         {
+            throw new Exception("Error Error Error!!!");
             var message = Server.HtmlEncode(name);
             return Content(message);
         }
---
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -0,0 +1,120 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            var model =
+                from r in _review
+                orderby r.Country
+                select r;
+
+            return View(model);
+        }
+
+        // GET: Reviews/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reviews/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reviews/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reviews/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reviews/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reviews/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        static List<RestaurantReview> _review = new List<RestaurantReview>
+        {
+            new RestaurantReview{
+                Id=1,
+                Name="Fisherman's Warf",
+                City="Bangalore",
+                Country="India",
+                Rating=10
+            },
+            new RestaurantReview{
+                Id=2,
+                Name="Modi Chiwala",
+                City="NY",
+                Country="USA",
+                Rating=10
+            },
+            new RestaurantReview{
+                Id=3,
+                Name="Sumit Dosawala",
+                City="London",
+                Country="UK",
+                Rating=10
+            },
+        };
+    }
+}
